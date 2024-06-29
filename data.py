3763a0c0a2f4513305a21ca9cd02389d2a171ef7
Aleksey Snatkin <snatkin.aleksey@gmail.com>
2024-06-29 18:11:39 +0300

    all done

---
+++ data.py
@@ -0,0 +1,18 @@
+
+class BunData:
+    bun_name = 'black bun'
+    bun_price = 100
+
+
+class IngredientData:
+    ingredient_name = 'chili sauce'
+    ingredient_price = 200
+    ingredient_type = 'SAUCE'
+
+
+class Receipt:
+    receipt_body = ('(==== black bun ====)\n'
+                    '= sauce chili sauce =\n'
+                    '(==== black bun ====)\n'
+                    '\n'
+                    'Price: 500')
